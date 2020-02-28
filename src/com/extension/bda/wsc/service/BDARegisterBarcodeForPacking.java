1d3b4d14d722305e3997d14a8c8049d3cbd1e598
Pat Faiola <pfaiola@us.ibm.com>
2020-02-28 14:37:17 -0500

    Update BDARegisterBarcodeForPacking.java

--- src/com/extension/bda/wsc/service/BDARegisterBarcodeForPacking.java
+++ src/com/extension/bda/wsc/service/BDARegisterBarcodeForPacking.java
@@ -1,29 +1,52 @@
 package com.extension.bda.wsc.service;
 
-import java.util.Properties;
-
-import org.w3c.dom.Document;
 
 import com.extension.bda.service.IBDAService;
 import com.yantra.pca.bridge.YCDFoundationBridge;
 import com.yantra.pca.ycd.business.YCDRegisterBarcodeForPacking;
 import com.yantra.pca.ycd.utils.YCDUtils;
+import com.yantra.pca.ycd.utils.YCDXMLUtils;
+import com.yantra.shared.dbi.YFS_Container_Details;
 import com.yantra.shared.dbi.YFS_Shipment;
 import com.yantra.shared.dbi.YFS_Shipment_Line;
+import com.yantra.shared.dbi.YFS_Shipment_Tag_Serial;
+import com.yantra.shared.ycd.YCDErrorCodes;
+import com.yantra.shared.ycp.YCPLiterals;
 import com.yantra.shared.ycp.YFSContext;
+import com.yantra.shared.ycp.sim.YCPSIMApiManager;
+import com.yantra.ycp.core.YCPTemplateManager;
 import com.yantra.yfc.core.YFCIterable;
 import com.yantra.yfc.core.YFCObject;
 import com.yantra.yfc.dom.YFCDocument;
 import com.yantra.yfc.dom.YFCElement;
+import com.yantra.yfc.dom.YFCNode;
 import com.yantra.yfc.log.YFCLogCategory;
+import com.yantra.yfc.ui.backend.util.APIManager;
 import com.yantra.yfc.util.YFCCommon;
 import com.yantra.yfc.util.YFCDoubleUtils;
 import com.yantra.yfc.util.YFCException;
+import com.yantra.yfc.util.YFCI18NUtils;
+import com.yantra.yfc.util.YFCLocale;
 import com.yantra.yfs.japi.YFSEnvironment;
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import org.w3c.dom.Document;
+
 
 public class BDARegisterBarcodeForPacking extends YCDRegisterBarcodeForPacking implements IBDAService {
 	private static YFCLogCategory cat = YFCLogCategory.instance(BDARegisterBarcodeForPacking.class.getName());
 	private boolean isNewContainer = false;
+	  private Object[] unpackNotebundleArgs = new Object[4];
+	  
+	  private boolean isSIMEnabled = false;
+	  
+	  private String scannedSerialNo = "";
+	  
+	  private String tagSerialKey = "";
 	private String lotNumber = null;
 	
 	@Override
@@ -48,6 +71,213 @@ public class BDARegisterBarcodeForPacking extends YCDRegisterBarcodeForPacking i
 		}
 		return registerBarcodeForPacking(env, inputElem.getOwnerDocument()).getDocument();
 	}
+	
+	  public YFCDocument registerBarcodeForPacking(YFSEnvironment oEnv, YFCDocument inDoc) {
+		    try {
+		      cat.beginTimer("registerBarcodeForPack");
+		      YFSContext ctx = (YFSContext)oEnv;
+		      if (cat.isVerboseEnabled())
+		        cat.verbose("Input to registerBarcodeForPack:" + inDoc); 
+		      YCDUtils.validateInputDocument(ctx, inDoc);
+		      setIsSIMEnabled(ctx, inDoc);
+		      YFCElement inputElem = inDoc.getDocumentElement();
+		      YFCElement shipmenContextualInfoElem = inputElem.getChildElement("ShipmentContextualInfo");
+		      String action = inputElem.getAttribute("Action");
+		      String sProductClass = null;
+		      if (YFCObject.isVoid(action) || YFCObject.equals(action, "PACK")) {
+		        inputElem.setAttribute("Action", "PACK");
+		        action = "PACK";
+		      } else if (YFCObject.equals(action, "UNPACK")) {
+		        action = "UNPACK";
+		      } else {
+		        YFCException ex = new YFCException("YFS10423");
+		        ex.setAttribute("Action", action);
+		        throw ex;
+		      } 
+		      YFCElement itemContextElem = inputElem.getChildElement("ItemContextualInfo");
+		      if (!YFCObject.isVoid(itemContextElem))
+		        sProductClass = itemContextElem.getAttribute("ProductClass"); 
+		      YFS_Shipment oShipment = YCDUtils.getShipment(ctx, shipmenContextualInfoElem);
+		      YFCDocument translateBarcodeOutDoc = YCDUtils.callTranslateBarcode(ctx, inputElem);
+		      YFCElement translateBarcodeOutput = translateBarcodeOutDoc.getDocumentElement();
+		      YCDUtils.validateTranslateBarcodeOutput(ctx, translateBarcodeOutput, inputElem, this.isSIMEnabled);
+		      YFS_Shipment_Line oMatchingLine = findMatchingShipmentLine(ctx, oShipment, translateBarcodeOutput, action, sProductClass);
+		      YFCDocument templateDoc = YCPTemplateManager.getInstance().getAPITemplate(ctx, "registerBarcodeForPacking", inDoc, new HashMap<>());
+		      YFCElement templateElem = templateDoc.getDocumentElement();
+		      YFCElement ouputTemplate = (YFCElement)templateElem.cloneNode(true);
+		      callChangeShipment(ctx, oShipment, oMatchingLine, shipmenContextualInfoElem, action, translateBarcodeOutput);
+		      YFCDocument outputDoc = prepareOutputDocument(ctx, translateBarcodeOutDoc, oShipment.getShipment_Key(), oMatchingLine.getShipment_Line_Key(), templateElem, shipmenContextualInfoElem);
+		      YFCElement note = inputElem.getChildElement("Note");
+		      if (!YFCElement.isVoid(note)) {
+		        String reasonCode = note.getAttribute("ReasonCode");
+		        String noteText = note.getAttribute("NoteText");
+		        if ("YCD_UNPACK_REASON".equals(reasonCode))
+		          noteText = getUnpackNoteText(oEnv, noteText, outputDoc.getDocumentElement()); 
+		        YCDFoundationBridge.getInstance().recordReasonNote((YFSContext)oEnv, "YFS_ORDER_HEADER", oMatchingLine.getOrder_Header_Key(), reasonCode, noteText);
+		      } 
+		      YCDXMLUtils.removeExtraEntities(outputDoc.getDocumentElement(), ouputTemplate);
+		      if (cat.isVerboseEnabled())
+		        cat.verbose("Output from registerBarcodeForPacking:" + outputDoc); 
+		      return outputDoc;
+		    } finally {
+		      cat.endTimer("registerBarcodeForPack");
+		    } 
+		  }
+	  
+	  private YFS_Shipment_Line findMatchingShipmentLine(YFSContext ctx,YFS_Shipment oShipment,YFCElement translateBarcodeOutput,String action,String productClass){
+			YFS_Shipment_Line oMatchingLine = null;
+			boolean scannedItemPresent = false;
+			boolean isQuantityValid =false;
+			try{
+				cat.beginTimer("findMatchingShipmentLine");
+				YFCElement translation = translateBarcodeOutput.getChildElement(YCPLiterals.YCP_BARCODE_TRANSLATIONS).getChildElement(YCPLiterals.YCP_BARCODE_TRANSLATION);
+				YFCElement itemContElem = translation.getChildElement(YCPLiterals.YCP_BARCODE_ITEM_CONTEXTUAL_INFO);
+				String sItemId = itemContElem.getAttribute(YCPLiterals.YCP_BARCODE_SOURCE_ITEM);
+				String sProductClass = itemContElem.getAttribute(YCPLiterals.YCP_COND_PRODUCT_CLASS);
+				sProductClass = itemContElem.getAttribute(YCPLiterals.YCP_COND_PRODUCT_CLASS);
+				if(YFCObject.isVoid(sProductClass)){
+					sProductClass = productClass ;
+				}
+				String sUom = itemContElem.getAttribute("InventoryUOM");
+				String sKitCode = itemContElem.getAttribute("KitCode");
+				double dQty = itemContElem.getDoubleAttribute(YFS_Shipment_Line.QUANTITY);
+				if(!YFCDoubleUtils.greaterThan(dQty,0)){
+					dQty = 1; //defaulting to 1 if a positive value is not returned.
+					itemContElem.setDoubleAttribute(YFS_Shipment_Line.QUANTITY,dQty);
+				}
+
+				List<YFS_Shipment_Line> lineList = oShipment.getShipmentLineList();
+				for(Iterator<YFS_Shipment_Line> ite = lineList.iterator();ite.hasNext();){
+
+					YFS_Shipment_Line oLine = ite.next();
+					double quantity = oLine.getQuantity();
+					double packedQuantity =oLine.getPackedQuantity();
+					double unpackedQuantity = quantity - packedQuantity;
+
+					if(!YFCObject.equals(oLine.getItem_Id(),sItemId)){
+						continue;
+					}
+
+					if(!YFCObject.isVoid(sProductClass) && !YFCObject.equals(oLine.getProduct_Class(),sProductClass)){
+						continue;
+					}
+
+					if(!YFCObject.isVoid(sUom) && !YFCObject.equals(oLine.getUom(),sUom)){
+						continue;
+					}
+					if(!YFCObject.isVoid(sKitCode) && !YFCObject.equals(oLine.getKit_Code(),sKitCode)){
+						continue;
+					}
+
+					scannedItemPresent = true;
+					if(!YFCObject.isVoid(oLine.getQuantity())  && action == "PACK"){
+						if (!YFCDoubleUtils.greaterThanEqualTo(unpackedQuantity,dQty)){
+							continue;
+						}
+					}
+
+					else if (!YFCObject.isVoid(oLine.getPackedQuantity()) && action == "UNPACK"){
+						if (!YFCDoubleUtils.lessThanEqualTo(dQty, packedQuantity)){
+							continue;
+						}
+					}
+					isQuantityValid = true;
+
+					oMatchingLine = oLine;
+
+					YCDUtils.appendShipmentLineInformationToOutput(ctx,oMatchingLine,translation);
+					break;
+
+				}
+
+				if(oMatchingLine == null && !scannedItemPresent){
+					YFCException ex = new YFCException(YCDErrorCodes.YCD_BARCODE_SCANNED_ITEM_DOES_NOT_BELONG_TO_SHIPMENT);
+					ex.setAttribute(YCPLiterals.YCP_BARCODE_SOURCE_ITEM,sItemId);
+					ex.setAttribute("UnitOfMeasure",sUom);
+					throw ex;
+				}else if(oMatchingLine == null && scannedItemPresent && !isQuantityValid && action=="PACK"){
+					YFCException ex = new YFCException(YCDErrorCodes.YCD_BARCODE_PACKED_QUANTITY_EXCEEDS_LINE_QUANTITY);
+					ex.setAttribute(YCPLiterals.YCP_BARCODE_SOURCE_ITEM,sItemId);
+					ex.setAttribute("UnitOfMeasure",sUom);
+					ex.setAttribute("ScannedQuantity",Double.toString(dQty));
+					throw ex;
+				}else if(oMatchingLine == null && scannedItemPresent && !isQuantityValid && action=="UNPACK"){
+					YFCException ex = new YFCException(YCDErrorCodes.YCD_BARCODE_UNPACKED_QUANTITY_EXCEEDS_LINE_QUANTITY);
+					ex.setAttribute(YCPLiterals.YCP_BARCODE_SOURCE_ITEM,sItemId);
+					ex.setAttribute("UnitOfMeasure",sUom);
+					ex.setAttribute("ScannedQuantity",Double.toString(dQty));
+					throw ex;
+				}
+				else{
+					return oMatchingLine;
+				}
+
+			}finally{
+				cat.endTimer("findMatchingShipmentLine");
+			}
+		}
+	  
+	  
+	  private void setIsSIMEnabled(YFSContext ctx, YFCDocument inDoc) {
+	    YFCElement oContextualInfo = inDoc.getDocumentElement().getChildElement("ContextualInfo");
+	    String inputNode = oContextualInfo.getAttribute("Node");
+	    if (!YFCObject.isVoid(inputNode))
+	      this.isSIMEnabled = YCPSIMApiManager.getInstance().isSIMEnabled(ctx, inputNode); 
+	  }
+	  
+	  private String getUnpackNoteText(YFSEnvironment env, String unpackReason, YFCElement output) {
+	    String noteText = "";
+	    this.unpackNotebundleArgs[3] = unpackReason;
+	    YFCElement translations = output.getChildElement("Translations");
+	    if (!YFCElement.isVoid(translations)) {
+	      YFCElement translation = translations.getChildElement("Translation");
+	      if (!YFCElement.isVoid(translation)) {
+	        YFCElement itemInfo = translation.getChildElement("ItemContextualInfo");
+	        if (!YFCElement.isVoid(itemInfo)) {
+	          this.unpackNotebundleArgs[0] = itemInfo.getAttribute("ItemID");
+	          this.unpackNotebundleArgs[1] = itemInfo.getAttribute("Quantity");
+	        } 
+	      } 
+	    } 
+	    YFCLocale alocale = getLoggedInUserYFCLocale(env);
+	    noteText = MessageFormat.format(YFCI18NUtils.getString("Unpack_Item_Note_Text", alocale), this.unpackNotebundleArgs);
+	    cat.verbose("Note :" + noteText);
+	    return noteText;
+	  }
+	  
+	  private YFCLocale getLoggedInUserYFCLocale(YFSEnvironment env) {
+	    YFCLocale alocale = null;
+	    if (env.getUserId() != null) {
+	      try {
+	        YFCDocument dGetUserHierarchy = YFCDocument.createDocument("User");
+	        YFCElement eGetUserHierarchy = dGetUserHierarchy.getDocumentElement();
+	        eGetUserHierarchy.setAttribute("Loginid", env.getUserId());
+	        YFCElement apiElem = YFCDocument.createDocument("API").getDocumentElement();
+	        apiElem.setAttribute("Name", "getUserHierarchy");
+	        YFCElement template = YFCDocument.createDocument("User").getDocumentElement();
+	        template.setAttribute("Localecode", "");
+	        YFCElement userDataElement = APIManager.getInstance().invokeAPI(env, apiElem, eGetUserHierarchy, template);
+	        alocale = YFCLocale.getYFCLocale(userDataElement.getAttribute("Localecode"));
+	      } catch (Exception e) {
+	        alocale = YFCLocale.getDefaultLocale();
+	      } 
+	    } else {
+	      alocale = YFCLocale.getDefaultLocale();
+	    } 
+	    return alocale;
+	  }
+	  
+		  
+	  private void createTagSerialInput(YFCElement contDetailElem, YFCElement lineElem) {
+	    YFCElement shpTagSerial = contDetailElem.createChild("ShipmentTagSerials").createChild("ShipmentTagSerial");
+	    shpTagSerial.setAttribute("SerialNo", this.scannedSerialNo);
+	    shpTagSerial.setAttribute("Quantity", "1");
+	    shpTagSerial.setAttribute("Action", "Create");
+	    YFCElement lineShpTagSerial = lineElem.createChild("ShipmentTagSerials").createChild("ShipmentTagSerial");
+	    lineShpTagSerial.setAttribute("Action", "Delete");
+	    lineShpTagSerial.setAttribute("ShipmentTagSerialKey", this.tagSerialKey);
+	  }
+	  
 	private YFCElement callChangeShipment(YFSContext ctx, YFS_Shipment oShipment, YFS_Shipment_Line oMatchingLine, YFCElement shipmentElem, String action, YFCElement translateBarcodeOutput) {
 	    try {
 	      cat.beginTimer("callChangeShipment");
@@ -202,5 +432,110 @@ public class BDARegisterBarcodeForPacking extends YCDRegisterBarcodeForPacking i
 	      cat.endTimer("callChangeShipment");
 	    } 
 	  }
-
+	private boolean serialAlreadyPacked(YFSContext ctx, YFS_Shipment oShipment) {
+	    List<YFS_Shipment_Line> lineList = oShipment.getShipmentLineList();
+	    for (Iterator<YFS_Shipment_Line> ite = lineList.iterator(); ite.hasNext(); ) {
+	      List<YFS_Container_Details> ctlDtl = ((YFS_Shipment_Line)ite.next()).getContainerDetailList();
+	      for (Iterator<YFS_Container_Details> oIter = ctlDtl.iterator(); oIter.hasNext(); ) {
+	        List<YFS_Shipment_Tag_Serial> oContainerDetailTagSerialList = ((YFS_Container_Details)oIter.next()).getShipmentTagSerialList();
+	        for (Iterator<YFS_Shipment_Tag_Serial> shpTagItr = oContainerDetailTagSerialList.iterator(); shpTagItr.hasNext();) {
+	          if (this.scannedSerialNo.equals(((YFS_Shipment_Tag_Serial)shpTagItr.next()).getSerial_No()))
+	            return true; 
+	        } 
+	      } 
+	    } 
+	    return false;
+	  }
+	  
+	  private void massageInstructionTextforShipmentLine(YFSContext ctx, YFCElement elemShipmentLine, YFS_Shipment_Line oShipmentLine) {
+	    cat.beginTimer("YCDRegisterBarcodeForPacking.massageInstructionTextforShipmentLine");
+	    if (!YFCCommon.isVoid(elemShipmentLine)) {
+	      boolean ispackInstructionfound = false;
+	      YFCElement instructionsElem = elemShipmentLine.getChildElement("Instructions");
+	      if (!YFCCommon.isVoid(instructionsElem)) {
+	        YFCIterable<YFCElement> it = instructionsElem.getChildren("Instruction");
+	        while (it.hasNext()) {
+	          YFCElement elemInstruction = (YFCElement)it.next();
+	          String instructionType = elemInstruction.getAttribute("InstructionType");
+	          if (!YFCCommon.isVoid(instructionType) && YFCCommon.equals(instructionType, "PACK", false)) {
+	            YFCElement elementInstruction = (YFCElement)elemInstruction.cloneNode(true);
+	            YFCDocument instructionsDoc = YFCDocument.createDocument("Instructions");
+	            YFCElement instructionsElement = instructionsDoc.getDocumentElement();
+	            instructionsElement.importNode((YFCNode)elementInstruction);
+	            instructionsElem.getParentNode().removeChild((YFCNode)instructionsElem);
+	            elemShipmentLine.importNode((YFCNode)instructionsElement);
+	            ispackInstructionfound = true;
+	            break;
+	          } 
+	          instructionsElem.removeChild((YFCNode)elemInstruction);
+	        } 
+	      } 
+	      if (!ispackInstructionfound) {
+	        YFCElement instructionListElem = null;
+	        YFCElement orderLineElem = elemShipmentLine.getChildElement("OrderLine");
+	        if (!YFCCommon.isVoid(orderLineElem)) {
+	          YFCElement itemDetailsElem = orderLineElem.getChildElement("ItemDetails");
+	          if (!YFCCommon.isVoid(itemDetailsElem))
+	            instructionListElem = itemDetailsElem.getChildElement("ItemInstructionList"); 
+	          if (!YFCCommon.isVoid(instructionListElem)) {
+	            YFCIterable<YFCElement> it = instructionListElem.getChildren("ItemInstruction");
+	            while (it.hasNext()) {
+	              YFCElement elemInstruction = (YFCElement)it.next();
+	              String instructionType = elemInstruction.getAttribute("InstructionType");
+	              if (!YFCCommon.isVoid(instructionType) && YFCCommon.equals(instructionType, "PACK", false)) {
+	                YFCElement instructionElement = instructionsElem.createChild("Instruction");
+	                instructionElement.setAttributes(elemInstruction.getAttributes());
+	                ispackInstructionfound = true;
+	                break;
+	              } 
+	            } 
+	          } 
+	        } 
+	      } 
+	    } 
+	    cat.endTimer("YCDRegisterBarcodeForPacking.massageInstructionTextforShipmentLine");
+	  }
+	  
+	  private YFCDocument prepareOutputDocument(YFSContext ctx, YFCDocument translateBarcodeOutDoc, String sShipmentKey, String sShipmentLineKey, YFCElement templateElem, YFCElement shipmenContextualInfoElem) {
+	    YFCElement shipmentTemplateElem = templateElem.getChildElement("Shipment");
+	    if (shipmentTemplateElem == null)
+	      return translateBarcodeOutDoc; 
+	    YFCElement shipmentLineTemplateElem = shipmentTemplateElem.getChildElement("ShipmentLine");
+	    YFCElement containerDetailsTemplateElem = shipmentLineTemplateElem.getChildElement("ContainerDetail");
+	    shipmentTemplateElem.removeChild((YFCNode)shipmentLineTemplateElem);
+	    YFCElement contDetailsOutput = null;
+	    YFS_Shipment shipmentObj = YCDFoundationBridge.getInstance().getShipment(ctx, sShipmentKey);
+	    YFS_Shipment_Line shipmentLineObj = shipmentObj.getShipmentLine(sShipmentLineKey);
+	    String shipmentContainerKey = shipmenContextualInfoElem.getAttribute("ShipmentContainerKey");
+	    List<YFS_Container_Details> contList = shipmentLineObj.getContainerDetailList();
+	    if (!YFCObject.isVoid(containerDetailsTemplateElem))
+	      if (this.isNewContainer) {
+	        String containerScm = shipmenContextualInfoElem.getAttribute("ContainerScm");
+	        for (Iterator<YFS_Container_Details> ite = contList.iterator(); ite.hasNext(); ) {
+	          YFS_Container_Details ocontDetails = ite.next();
+	          if (YFCObject.equals(containerScm, ocontDetails.getShipmentContainer().getContainer_Scm())) {
+	            contDetailsOutput = ocontDetails.getXML(YFCDocument.createDocument("ContainerDetails"), containerDetailsTemplateElem);
+	            break;
+	          } 
+	        } 
+	      } else {
+	        for (Iterator<YFS_Container_Details> ite = contList.iterator(); ite.hasNext(); ) {
+	          YFS_Container_Details ocontDetails = ite.next();
+	          if (YFCObject.equals(shipmentContainerKey, ocontDetails.getShipment_Container_Key())) {
+	            contDetailsOutput = ocontDetails.getXML(YFCDocument.createDocument("ContainerDetails"), containerDetailsTemplateElem);
+	            break;
+	          } 
+	        } 
+	      }  
+	    YFCElement shipmentOutput = shipmentObj.getXML(YFCDocument.createDocument("Shipment"), shipmentTemplateElem);
+	    YFCElement shipmentLineOutput = shipmentLineObj.getXML(YFCDocument.createDocument("ShipmentLine"), shipmentLineTemplateElem);
+	    massageInstructionTextforShipmentLine(ctx, shipmentLineOutput, shipmentLineObj);
+	    if (!YFCObject.isVoid(contDetailsOutput))
+	      shipmentLineOutput.importNode((YFCNode)contDetailsOutput); 
+	    shipmentOutput.importNode((YFCNode)shipmentLineOutput);
+	    translateBarcodeOutDoc.getDocumentElement().importNode((YFCNode)shipmentOutput);
+	    if (this.isSIMEnabled && !YFCObject.isVoid(this.scannedSerialNo))
+	      translateBarcodeOutDoc.getDocumentElement().setAttribute("ScannedSerialNo", this.scannedSerialNo); 
+	    return translateBarcodeOutDoc;
+	  }
 }
